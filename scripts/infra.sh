33949666dceb225b9f78c90b81a5af349c142567
Foivos Zakkak <zakkak@ibm.com>
2026-03-11 23:27:10 +0200

    Reduce wait time...

--- scripts/infra.sh
+++ scripts/infra.sh
@@ -83,7 +83,7 @@ start_postgres() {
   echo "PostgreSQL DB process: $pid"
 
   echo "Waiting for PostgreSQL to be ready..."
-  timeout 90s bash -c "until ${engine} exec $DB_CONTAINER_NAME pg_isready -h localhost -U fruits; do sleep 5 ; done" || {
+  timeout 90s bash -c "until ${engine} exec $DB_CONTAINER_NAME pg_isready -h localhost -U fruits; do sleep 1 ; done" || {
     echo "Error: PostgreSQL failed to become ready"
     exit 1
   }
